85b67428e1c47caab7aa17520bc125c7e726a15e
ArcticWalrus <malcolm19@hotmail.com>
2020-01-26 02:02:19 -0500

    Final version of calls to api

--- api_calls/octave.py
+++ api_calls/octave.py
@@ -5,13 +5,14 @@ import json
 import requests
 import os
 import wget
+import pwd
+import grp
 
 
 class Octave:
     def __init__(self):
         self.payload = {}
         self.headers = {'Authorization': '51fal5fybpmqk0i2v1potkxoh1neqbu1'}
-        self.source_path = '{}/music/'.format(os.getenv('APP_PATH'))
 
     def getSongs(self, inputString):
         song_title = self.refactorSongName(inputString)
@@ -19,18 +20,18 @@ class Octave:
         full_list = requests.request("GET", url, headers=self.headers, data=self.payload)
         data = json.loads(full_list.content)
         song_list = data['songs']
+        print(len(song_list))
         if not song_list:
             print('No song found with that name')
-            return 1
+            return '', ''
         song = song_list[0]
-        self.getSong(song['id'], song['title'])
-        return 0
+        return self.getSong(song['id'], song['title'])
 
     def getSong(self, song_id, song_title):
         url = "https://conuhacks-2020.tsp.cld.touchtunes.com/v1/songs/" + str(song_id)
         song = json.loads(requests.request("GET", url, headers=self.headers, data=self.payload).content)
         print(song['playUrl'])
-        self.download(song['playUrl'], song_title)
+        return self.download(song['playUrl'], song_title)
 
     def refactorSongName(self, song_title):
         title = song_title.split(' ')
@@ -40,12 +41,10 @@ class Octave:
 
     def download(self, url, song_title):
         print('Downloading file:', song_title)
-        #wget.download(url, out='{}{}'.format(self.source_path, song_title))
-        filename = wget.download(url)
-        #print(filename)
-        return filename
+        filename = wget.download(url, out='music/')
+        return filename, song_title
 
 
 if __name__ == "__main__":
     octave = Octave()
-    octave.getSongs("hello there")
+    octave.getSongs("belong with me")
