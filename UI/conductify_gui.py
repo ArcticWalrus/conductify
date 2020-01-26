4cd7b927363056a73b00bb544c5f6aa1c1879979
GabrielElkadiki <46852673+GabrielElkadiki@users.noreply.github.com>
2020-01-26 07:52:52 -0500

    Added GUI

--- UI/conductify_gui.py
+++ UI/conductify_gui.py
@@ -14,12 +14,47 @@ class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(1128, 863)
-        self.pushButton = QtWidgets.QPushButton(Form)
-        self.pushButton.setGeometry(QtCore.QRect(120, 760, 291, 91))
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(Form)
-        self.pushButton_2.setGeometry(QtCore.QRect(720, 760, 291, 91))
-        self.pushButton_2.setObjectName("pushButton_2")
+        Form.setStyleSheet("background-color: #6898a4;")
+        # self.searchbar = QtWidgets.QLineEdit(Form)
+        # self.searchbar.setGeometry(QtCore.QRect(100, 500, 291, 91))
+        # self.searchbar.setObjectName("searchbar")
+        self.start_pushButton = QtWidgets.QPushButton(Form)
+        self.start_pushButton.setGeometry(QtCore.QRect(120, 760, 291, 91))
+        self.start_pushButton.setObjectName("start_pushButton")
+        self.start_pushButton.setStyleSheet("""
+            QPushButton {
+                background-color: green;
+                border-style: outset;
+                border-width: 2px;
+                border-radius: 10px;
+                border-color: beige;
+                font: bold 30px;
+                min-width: 10em;
+                padding: 6px;
+                }
+            QPushButton:pressed {
+                background-color: rgb(0, 250, 0);
+                border-style: inset;
+        }""")
+        self.stop_pushButton = QtWidgets.QPushButton(Form)
+        self.stop_pushButton.setGeometry(QtCore.QRect(720, 760, 291, 91))
+        self.stop_pushButton.setObjectName("stop_pushButton")
+        self.stop_pushButton.setStyleSheet("""
+            QPushButton {
+                background-color: rgb(125, 0, 0);
+                border-style: outset;
+                border-width: 2px;
+                border-radius: 10px;
+                border-color: beige;
+                font: bold 30px;
+                min-width: 10em;
+                padding: 6px;
+            }
+            QPushButton:pressed {
+                background-color: rgb(250, 0, 0);
+                border-style: inset;
+        }""")
+
         self.horizontalLayoutWidget = QtWidgets.QWidget(Form)
         self.horizontalLayoutWidget.setGeometry(QtCore.QRect(30, 20, 1081, 541))
         self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
@@ -27,39 +62,92 @@ class Ui_Form(object):
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.otherBar = QtWidgets.QProgressBar(self.horizontalLayoutWidget)
-        self.otherBar.setProperty("value", 24)
+        self.otherBar.setProperty("value", 100)
         self.otherBar.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTop|QtCore.Qt.AlignTrailing)
         self.otherBar.setTextVisible(False)
         self.otherBar.setOrientation(QtCore.Qt.Vertical)
         self.otherBar.setObjectName("otherBar")
+        self.otherBar.setStyleSheet("""
+                                QProgressBar {
+                                    border: 6px solid grey;
+                                    border-radius: 7px;
+
+                                }
+                                QProgressBar::chunk {
+                                    background-color: #0064c8
+                                }
+        """)
         self.horizontalLayout.addWidget(self.otherBar)
         self.drumsBar = QtWidgets.QProgressBar(self.horizontalLayoutWidget)
-        self.drumsBar.setProperty("value", 24)
+        self.drumsBar.setProperty("value", 100)
         self.drumsBar.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTop|QtCore.Qt.AlignTrailing)
         self.drumsBar.setTextVisible(False)
         self.drumsBar.setOrientation(QtCore.Qt.Vertical)
         self.drumsBar.setObjectName("drumsBar")
+        self.drumsBar.setStyleSheet("""
+                                QProgressBar {
+                                    border: 6px solid grey;
+                                    border-radius: 7px;
+
+                                }
+                                QProgressBar::chunk {
+                                    background-color: #0064c8
+                                }
+        """)
+
         self.horizontalLayout.addWidget(self.drumsBar)
         self.pianoBar = QtWidgets.QProgressBar(self.horizontalLayoutWidget)
-        self.pianoBar.setProperty("value", 24)
+        self.pianoBar.setProperty("value", 100)
         self.pianoBar.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTop|QtCore.Qt.AlignTrailing)
         self.pianoBar.setTextVisible(False)
         self.pianoBar.setOrientation(QtCore.Qt.Vertical)
         self.pianoBar.setObjectName("pianoBar")
+        self.pianoBar.setStyleSheet("""
+                                QProgressBar {
+                                    border: 6px solid grey;
+                                    border-radius: 7px;
+
+                                }
+                                QProgressBar::chunk {
+                                    background-color: #0064c8
+                                }
+        """)
         self.horizontalLayout.addWidget(self.pianoBar)
         self.bassBar = QtWidgets.QProgressBar(self.horizontalLayoutWidget)
-        self.bassBar.setProperty("value", 24)
+        self.bassBar.setProperty("value", 100)
         self.bassBar.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTop|QtCore.Qt.AlignTrailing)
         self.bassBar.setTextVisible(False)
         self.bassBar.setOrientation(QtCore.Qt.Vertical)
         self.bassBar.setObjectName("bassBar")
+        self.bassBar.setStyleSheet("""
+                                QProgressBar {
+                                    border: 6px solid grey;
+                                    border-radius: 7px;
+
+                                }
+                                QProgressBar::chunk {
+                                    background-color: #0064c8
+                                }
+        """)
+
         self.horizontalLayout.addWidget(self.bassBar)
         self.vocalsBar = QtWidgets.QProgressBar(self.horizontalLayoutWidget)
-        self.vocalsBar.setProperty("value", 24)
+        self.vocalsBar.setProperty("value", 100)
         self.vocalsBar.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTop|QtCore.Qt.AlignTrailing)
         self.vocalsBar.setTextVisible(False)
         self.vocalsBar.setOrientation(QtCore.Qt.Vertical)
         self.vocalsBar.setObjectName("vocalsBar")
+        self.vocalsBar.setStyleSheet("""
+                                QProgressBar {
+                                    border: 6px solid grey;
+                                    border-radius: 7px;
+
+                                }
+                                QProgressBar::chunk {
+                                    background-color: #0064c8
+                                }
+        """)
+
         self.horizontalLayout.addWidget(self.vocalsBar)
         self.otherLabel = QtWidgets.QLabel(Form)
         self.otherLabel.setGeometry(QtCore.QRect(140, 590, 101, 141))
@@ -81,19 +169,19 @@ class Ui_Form(object):
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
-        pixOther = QPixmap('other.png')
+        pixOther = QPixmap('UI/other.png')
         pixOther = pixOther.scaled(self.otherLabel.width(), self.otherLabel.height(), QtCore.Qt.KeepAspectRatio)
 
-        pixDrums = QPixmap('drums.png')
+        pixDrums = QPixmap('UI/drums.png')
         pixDrums = pixDrums.scaled(self.drumLabel.width(), self.drumLabel.height(), QtCore.Qt.KeepAspectRatio)
 
-        pixBass = QPixmap('bass.png')
+        pixBass = QPixmap('UI/bass.png')
         pixBass = pixBass.scaled(self.bassLabel.width(), self.bassLabel.height(), QtCore.Qt.KeepAspectRatio)
 
-        pixPiano = QPixmap("piano.png")
+        pixPiano = QPixmap("UI/piano.png")
         pixPiano = pixPiano.scaled(self.pianoLabel.width(), self.pianoLabel.height(), QtCore.Qt.KeepAspectRatio)
 
-        pixVocals = QPixmap("vocals.png")
+        pixVocals = QPixmap("UI/vocals.png")
         pixVocals = pixVocals.scaled(self.vocalsLabel.width(), self.vocalsLabel.height(), QtCore.Qt.KeepAspectRatio)
 
         self.otherLabel.setPixmap(pixOther)
@@ -114,8 +202,8 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
-        self.pushButton.setText(_translate("Form", "Play"))
-        self.pushButton_2.setText(_translate("Form", "Pause"))
+        self.start_pushButton.setText(_translate("Form", " PLAY"))
+        self.stop_pushButton.setText(_translate("Form", "STOP"))
         self.drumLabel.setText(_translate("Form", "piano"))
         self.pianoLabel.setText(_translate("Form", "drums"))
         self.bassLabel.setText(_translate("Form", "bass"))
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from PyQt5 import QtWidgets
+from UI import conductify_gui
+import sys
+from PyQt5.QtCore import QTimer
 import pygame
 import serial
 
@@ -7,10 +11,44 @@ pygame.init()
 ser = serial.Serial('COM18', baudrate=115200, timeout=1)
 wav_list = ["vocals.wav", "bass.wav", "drums.wav", "piano.wav", "other.wav"]
 
+non_select = """
+    QProgressBar {
+        border: 6px solid grey;
+        border-radius: 7px;
+
+    }
+    QProgressBar::chunk {
+        background-color: #0064c8
+    }
+"""
+
+select = """
+    QProgressBar {
+        border: 6px solid grey;
+        border-radius: 7px;
+
+    }
+    QProgressBar::chunk {
+        background-color: red
+    }
+"""
+
+pygame.mixer.set_num_channels(26)
+sound_arr = []
+for wav in wav_list:
+    print('spleet/' + wav)
+    sound_arr.append(pygame.mixer.Sound('spleet/' + wav))
+
+
+app = QtWidgets.QApplication(sys.argv)
+main_window_Qdialog = QtWidgets.QDialog()
+ui = conductify_gui.Ui_Form()
+ui.setupUi(main_window_Qdialog)
+main_window_Qdialog.show()
+
 
 def read_ser(port):
-    port.flushOutput()
-    port.write(b'')
+    port.write(b'.')
     data_raw = port.readline().decode().strip()
     num_arr = []
     if data_raw:
@@ -20,21 +58,63 @@ def read_ser(port):
         return num_arr
 
 
-pygame.mixer.set_num_channels(26)
+def start():
+    for i in range(4):
+        sound_arr[i].play()
 
-sound_arr = []
-for wav in wav_list:
-    sound_arr.append(pygame.mixer.Sound(wav))
 
-for i in range(4):
-    sound_arr[i].play()
+def stop():
+    if sound_arr:
+        for i in range(4):
+            sound_arr[i].stop()
 
-while 1:
+
+def change_bar_color(num):
+    ui.otherBar.setStyleSheet(non_select)
+    ui.otherBar.setProperty("value", sound_arr[0].get_volume()*100)
+
+    ui.drumsBar.setStyleSheet(non_select)
+    ui.drumsBar.setProperty("value", sound_arr[1].get_volume()*100)
+
+    ui.pianoBar.setStyleSheet(non_select)
+    ui.pianoBar.setProperty("value", sound_arr[2].get_volume()*100)
+
+    ui.bassBar.setStyleSheet(non_select)
+    ui.bassBar.setProperty("value", sound_arr[3].get_volume()*100)
+
+    ui.vocalsBar.setStyleSheet(non_select)
+    ui.vocalsBar.setProperty("value", sound_arr[4].get_volume()*100)
+
+    if num == 0:
+        ui.otherBar.setStyleSheet(select)
+    elif num == 1:
+        ui.drumsBar.setStyleSheet(select)
+    elif num == 2:
+        ui.pianoBar.setStyleSheet(select)
+    elif num == 3:
+        ui.bassBar.setStyleSheet(select)
+    elif num == 4:
+        ui.vocalsBar.setStyleSheet(select)
+
+
+def control_volume():
     control = read_ser(ser)
     if control is not None:
+        change_bar_color(int(control[2]))
         if control[0] == 1:
             if not control[2] == 255:
                 sound_arr[int(control[2])].set_volume(control[1])
                 print(control)
             else:
                 print("Out of Range")
+
+
+main_window_Qdialog.qTimer = QTimer()
+main_window_Qdialog.qTimer.setInterval(1)
+main_window_Qdialog.qTimer.timeout.connect(control_volume)
+
+main_window_Qdialog.qTimer.start()
+ui.start_pushButton.clicked.connect(start)
+ui.stop_pushButton.clicked.connect(stop)
+
+sys.exit(app.exec_())
